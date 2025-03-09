5db9eacdd30e3faa1fcda7bdb12f4ef8e0f1c869
Juan Rios <jd.riosn1@uniandes.edu.co>
2025-03-09 10:01:10 -0500

    NavegationBar

---
+++ lib/views/Pagina1.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pagina1 extends StatelessWidget {
+  const Pagina1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Página 1', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
