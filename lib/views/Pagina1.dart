4a2c33705ce9c9d2fbb73b2f2ee2d9b02d39ab96
Juan Rios <jd.riosn1@uniandes.edu.co>
2025-03-12 21:17:44 -0500

    Add Inter variable font and license files

--- lib/views/Pagina1.dart
+++ lib/views/Pagina1.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 import '../widgets/OvalsPainter.dart';
 
-class Pagina1 extends StatelessWidget {
+class Pagina1 extends StatefulWidget {
   const Pagina1({super.key});
 
   @override
+  _Pagina1State createState() => _Pagina1State();
+}
+
+class _Pagina1State extends State<Pagina1> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text('Hi, Juan', style: TextStyle(color: Colors.white, 
+        fontSize: 28, fontFamily: 'Inter', 
+        fontWeight: FontWeight.w600)), // Texto a la izquierda
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 30.0), // Espaciado derecho
+            child: ClipOval(
+            child: Image.asset(
+              'assets/icon.jpg',
+              width: 55, 
+              height: 55,
+              fit: BoxFit.cover, 
+            ),
+            ),
+          ),
+        ],
+      ),
       body: Stack(
         children: [
-          // Fondo con los círculos superpuestos
           Positioned.fill(
             child: CustomPaint(
               painter: OvalsPainter(),
             ),
           ),
-
-          // Contenido principal
           const Center(
             child: Text(
               'Página 1',
