c1b30b2636b10d7ee98d35b33e020a8e1b975b05
SamuelHernandez5 <sa.hernandezd1@uniandes.edu.co>
2025-03-27 23:17:51 -0500

    add connection to posthog

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:dart_g12/presentation/views/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:dart_g12/presentation/views/started_page.dart';
+import 'package:posthog_flutter/posthog_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +11,12 @@ void main() async {
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkdmRlYmliZWdneWNqYWV5cGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzMzYxNTEsImV4cCI6MjA1NzkxMjE1MX0.bUhJdVbWwo018EzJfEdkHuK6ZqaTrXlys07Kb6CTTFM',
   );
+
+  final config = PostHogConfig('phx_doPSkw2mt705QFJg38ru3XsAh77ulHN0NMRnThadLkU5u91');
+  config.debug = true;
+  config.captureApplicationLifecycleEvents = true;
+  config.host = 'https://us.i.posthog.com';
+  await Posthog().setup(config);
   runApp(const MyApp());
 }
 
