601669eacb7db6f6e1bef21adc29042d52e7b84b
Juan Rios <jd.riosn1@uniandes.edu.co>
2025-04-03 00:57:44 -0500

    yaquedomimimim

--- lib/presentation/widgets/category_list.dart
+++ lib/presentation/widgets/category_list.dart
@@ -1,22 +1,35 @@
+import 'package:dart_g12/presentation/views/see_all_screen.dart';
 import 'package:flutter/material.dart';
-import 'category_icon.dart';  // Asegúrate de que este archivo esté importado
+import 'category_icon.dart';
 
 class CategoryList extends StatelessWidget {
-  const CategoryList({Key? key}) : super(key: key);
+  const CategoryList({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 70,
       child: ListView(
-      
         scrollDirection: Axis.horizontal,
-        children: const [
-          CategoryIcon(icon: Icons.business, label: "Buildings"),
-          CategoryIcon(icon: Icons.event, label: "Events"),
-          CategoryIcon(icon: Icons.restaurant, label: "Food & Rest"),
-          CategoryIcon(icon: Icons.school, label: "Study Spaces"),
-          CategoryIcon(icon: Icons.build, label: "Services"),
+        children: [
+          CategoryIcon(
+            icon: Icons.business,
+            label: "Buildings",
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SeeAllScreen()
+                ),
+              );
+            },
+          ),
+          const CategoryIcon(icon: Icons.event, label: "Events"),
+          const CategoryIcon(icon: Icons.restaurant, label: "Food & Rest"),
+          const CategoryIcon(icon: Icons.school, label: "Study Spaces"),
+          const CategoryIcon(icon: Icons.build, label: "Services"),
         ],
       ),
     );
