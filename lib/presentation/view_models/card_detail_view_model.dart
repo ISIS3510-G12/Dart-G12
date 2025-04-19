0bb7967f702533da84c21c724f87e6927ed32a3d
lauths12 <l.carretero@uniandes.edu.co>
2025-04-18 19:54:36 -0500

    Add CombinedViewModel and DetailCard for event and building details handling; implement fetching and displaying logic with improved UI structure.

---
+++ lib/presentation/view_models/card_detail_view_model.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import '../../data/repositories/event_repository.dart';
+import '../../data/repositories/location_repository.dart';
+import '../views/main_screen.dart';
+import '../../data/services/analytics_service.dart';
+
+class CombinedViewModel extends ChangeNotifier {
+  final EventRepository eventRepository = EventRepository();
+  final LocationRepository locationRepository = LocationRepository();
+
+  List<Map<String, dynamic>> _events = [];
+  Map<String, dynamic>? _event;  // Detalles del evento
+  Map<String, dynamic>? _building;  // Detalles del edificio
+  List<Map<String, dynamic>> _places = [];
+  bool _isLoading = false;
+  String? _error;
+  int _selectedIndex = 0;
+  final Map<int, Map<String, dynamic>> _buildingCache = {};
+
+  CombinedViewModel();
+
+  // Getters
+  List<Map<String, dynamic>> get events => _events;
+  Map<String, dynamic>? get event => _event;
+  Map<String, dynamic>? get building => _building;
+  List<Map<String, dynamic>> get places => _places;
+  bool get isLoading => _isLoading;
+  String? get error => _error;
+  int get selectedIndex => _selectedIndex;
+
+  // Métodos para manejar eventos
+  Future<void> fetchEvents() async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      _events = await eventRepository.fetchEvents();
+    } catch (e) {
+      _error = e.toString();
+    }
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> fetchEventDetails(int eventId) async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      _event = await eventRepository.fetchEventById(eventId);
+
+      await AnalyticsService.logUserAction(
+        actionType: 'consult_event',
+        eventId: eventId,
+        eventType: _event?['type'],
+        title: _event?['title'],
+        locationId: _event?['location_id'],
+      );
+    } catch (e) {
+      _error = e.toString();
+    }
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  // Métodos para manejar edificios y lugares
+  Future<void> fetchBuildingDetails(int buildingId) async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      if (_buildingCache.containsKey(buildingId)) {
+        _building = _buildingCache[buildingId];
+      } else {
+        _building = await locationRepository.fetchLocationById(buildingId);
+        if (_building == null) {
+          throw Exception('No se encontró el edificio.');
+        }
+        _buildingCache[buildingId] = _building!;
+      }
+
+      _places = await locationRepository.fetchPlacesByLocation(buildingId);
+    } catch (e) {
+      _error = e.toString();
+    }
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  // Actualizar el índice seleccionado
+  void updateSelectedIndex(int index) {
+    _selectedIndex = index;
+    notifyListeners();
+  }
+
+  // Navegar a la página de mapa
+  void goToMapPage(BuildContext context) {
+    updateSelectedIndex(2); // Índice 2 es para la página del mapa
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const MainScreen(initialIndex: 2),
+      ),
+    );
+  }
+
+  // Navegar a la página principal con el índice seleccionado
+  void onItemTapped(BuildContext context, int index) {
+    _selectedIndex = index;
+    notifyListeners();
+    Navigator.popUntil(context, (route) => route.isFirst);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MainScreen(initialIndex: index),
+      ),
+    );
+  }
+}
